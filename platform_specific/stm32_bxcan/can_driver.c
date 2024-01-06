e1f4876e65fedb47924931d55294a84014439cdd
Dmitry Ponomarev <36133264+PonomarevDA@users.noreply.github.com>
2024-01-06 20:58:09 +0300

    add bxcan and fdcan platforms (#1)

---
+++ platform_specific/stm32_bxcan/can_driver.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 Dmitry Ponomarev <ponomarevda96@gmail.com>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "can_driver.h"
+#include "canard_stm32.h"
+#include "main.h"
+
+int16_t canDriverInit(uint32_t can_speed, uint8_t can_driver_idx) {
+    CanardSTM32CANTimings timings;
+    int16_t res;
+
+    res = canardSTM32ComputeCANTimings(HAL_RCC_GetPCLK1Freq(), can_speed, &timings);
+    if (res < CANARD_OK) {
+        return res;
+    }
+
+    res = canardSTM32Init(&timings, CanardSTM32IfaceModeNormal);
+    if (res < CANARD_OK) {
+        return res;
+    }
+
+    return 0;
+}
+
+int16_t canDriverReceive(CanardCANFrame* const rx_frame, uint8_t can_driver_idx) {
+    return canardSTM32Receive(rx_frame);
+}
+
+int16_t canDriverTransmit(const CanardCANFrame* const tx_frame, uint8_t can_driver_idx) {
+    return canardSTM32Transmit(tx_frame);
+}
+
+uint64_t canDriverGetErrorCount() {
+    return canardSTM32GetStats().error_count;
+}
+
+uint64_t canDriverGetRxOverflowCount() {
+    return canardSTM32GetStats().rx_overflow_count;
+}
