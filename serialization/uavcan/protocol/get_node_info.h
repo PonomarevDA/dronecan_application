6409a68334c33b4eb6538cb4e9919e469897327c
PonomarevDA <ponomarevda96@gmail.com>
2024-03-18 20:47:07 +0300

    fix a few sonarcloud issues

--- serialization/uavcan/protocol/get_node_info.h
+++ serialization/uavcan/protocol/get_node_info.h
@@ -11,6 +11,7 @@
 #include <string.h>
 #include "dronecan_application_internal.h"
 #include "node_status.h"
+#include "serialization_internal.h"
 
 
 #define UAVCAN_GET_NODE_INFO_DATA_TYPE_ID           1
@@ -40,30 +41,34 @@ extern "C" {
 
 static inline uint16_t uavcanEncodeParamGetNodeInfo(
                         uint8_t buffer[UAVCAN_GET_NODE_INFO_RESPONSE_MAX_SIZE],
-                        const NodeStatus_t* node_status,
-                        const SoftwareVersion* sw_version,
-                        const HardwareVersion* hw_version,
-                        const char* node_name) {
+                        const NodeStatus_t* status,
+                        const SoftwareVersion* software,
+                        const HardwareVersion* hardware,
+                        const char* name) {
+    if (buffer == NULL || status == NULL || software == NULL || hardware == NULL || name == NULL) {
+        return 0;
+    }
+
     // 1. NodeStatus status. 7 bytes
-    uavcanEncodeNodeStatus(buffer, node_status);
+    uavcanEncodeNodeStatus(buffer, status);
 
     // 2. SoftwareVersion software_version. 15 bytes
-    buffer[7] = sw_version->major;
-    buffer[8] = sw_version->minor;
+    buffer[7] = software->major;
+    buffer[8] = software->minor;
     buffer[9] = 1;                              ///< optional_field_flags, VCS commit is set
-    uint32_t u32 = sw_version->vcs_commit;
+    uint32_t u32 = software->vcs_commit;
     canardEncodeScalar(buffer, 80, 32, &u32);
     memset(buffer + 14, 0x00, 8);    ///< uint64 image_crc
 
     // 3. HardwareVersion hardware_version. 18 bytes
-    buffer[22] = hw_version->major;
-    buffer[23] = hw_version->minor;
-    memcpy(&buffer[24], hw_version->unique_id, 16);
+    buffer[22] = hardware->major;
+    buffer[23] = hardware->minor;
+    memcpy(&buffer[24], hardware->unique_id, 16);
 
     // 4. Node name. Up to 80 bytes
-    size_t name_len = strlen(node_name);
+    size_t name_len = strlenSafely(name, 80);
     buffer[40] = 0;
-    memcpy(&buffer[41], node_name, name_len);
+    memcpy(&buffer[41], name, name_len);
 
     return 41 + name_len;
 }
