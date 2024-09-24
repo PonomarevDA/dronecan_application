2c0d414cd99be3d582c8a0009a7cd7871fe5875d
PonomarevDA <ponomarevda96@gmail.com>
2024-09-24 22:45:26 +0300

    fix code style issues

--- include/serialization/logging.h
+++ include/serialization/logging.h
@@ -8,24 +8,24 @@
 
 #include "uavcan/protocol/debug/LogMessage.h"
 
-static inline bool set_source(DebugLogMessage_t &msg, const char *str) {
+static inline bool set_source(DebugLogMessage_t* msg, const char *str) {
     if (str == nullptr) {
         return false;
     }
 
-    msg.source_size = strnlen(str, 31);
-    msg.source[30] = '\0';
-    memcpy((char*) msg.source, str,  msg.source_size);
+    msg->source_size = strnlen(str, 31);
+    msg->source[30] = '\0';
+    memcpy((char*) msg->source, str,  msg->source_size);
     return true;
 }
 
-static inline bool set_text(DebugLogMessage_t &msg, const char *str) {
+static inline bool set_text(DebugLogMessage_t* msg, const char *str) {
     if (str == nullptr) {
         return false;
     }
 
-    msg.text_size = strnlen(str, 90);
-    memcpy((char*) msg.text, str,  msg.text_size);
+    msg->text_size = strnlen(str, 90);
+    memcpy((char*) msg->text, str,  msg->text_size);
     return true;
 }
 
